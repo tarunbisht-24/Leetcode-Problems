a60156c80b0cab74a1d6a8e966d204fd6adb6766
Tarun Bisht <bishttarun2002@gmail.com>
2022-06-10 18:53:51 +0530

    Update Intersection of Two Linked Lists.cpp

--- src/Intersection of Two Linked Lists.cpp
+++ src/Intersection of Two Linked Lists.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-		if(headA == nullptr || headB == nullptr)return nullptr;
+	if(headA == nullptr || headB == nullptr)return nullptr;
 
-		ListNode* pA = headA, *pB = nullptr;
-		while(pA != nullptr)
-		{
-			pB = headB;           
-			while(pB != nullptr)
-			{
-				if(pA == pB)return pA;
-				pB = pB -> next;             
-			}
-			pA = pA -> next;        
-		}
+	ListNode* pA = headA, *pB = nullptr;
+	while(pA != nullptr)
+	  {
+		pB = headB;           
+		while(pB != nullptr)
+	  {
+		if(pA == pB)return pA;
+			pB = pB -> next;             
+	  }
+		pA = pA -> next;        
+	  }
 
-		return nullptr;        
+	return nullptr;        
 	}
 };
