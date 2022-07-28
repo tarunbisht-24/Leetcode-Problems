409a2a6c6f48048e109a94b264029a69524a0117
Tarun Bisht <bishttarun2002@gmail.com>
2022-07-28 20:43:21 +0530

    Create 242. Valid Anagram.cpp

---
+++ src/July-2022/242. Valid Anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] != t[i] || s.length()!= t.length())
+            {
+                return false;
+                break;
+            }            
+        }
+        return true;
+    }
+};
